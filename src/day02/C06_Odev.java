3cfa7971ce8608d260393148d14b60f3bb076e08
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-11 17:35:34 +0200

    java

---
+++ src/day02/C06_Odev.java
@@ -0,0 +1,23 @@
+package day02;
+
+public class C06_Odev {
+    public static void main(String[] args) {
+        /*
+         Kullanicidan bir dikdortgenin 2 kenar uzunlugunu alip,
+        dikdortgenin alanini yazdirin.
+
+         */
+
+        /*
+
+    Kullanicidan iki sayi alip
+    ikisinin degerlerini degistirin(swap).
+
+        kullanicinin girdigi degerler
+        sayi1= 10  sayi2=20 ise
+
+        siz kod ile bunlarin degerlerini degistirip
+        sayi1=20  sayi2=10 yapin
+     */
+    }
+}
