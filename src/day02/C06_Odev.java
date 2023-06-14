b8d6abe20efb6d6076001e07175300fec20f5563
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-14 18:30:46 +0200

    java

--- src/day02/C06_Odev.java
+++ src/day02/C06_Odev.java
@@ -1,5 +1,7 @@
 package day02;
 
+import java.util.Scanner;
+
 public class C06_Odev {
     public static void main(String[] args) {
         /*
@@ -7,6 +9,13 @@ public class C06_Odev {
         dikdortgenin alanini yazdirin.
 
          */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir dikdortgen icin 2 kenar uzunkugu belirtin");
+        double kenar1=scan.nextDouble();
+        double kenar2 = scan.nextDouble();
+
+        System.out.println("Dikdortgenin alanı: "+ kenar1*kenar2);
 
         /*
 
@@ -19,5 +28,19 @@ public class C06_Odev {
         siz kod ile bunlarin degerlerini degistirip
         sayi1=20  sayi2=10 yapin
      */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen iki sayi girin");
+        int kirmiziKova=scanner.nextInt();
+        int sariKova=scanner.nextInt();
+
+        int bosKova;
+
+        bosKova=kirmiziKova;
+        kirmiziKova=sariKova;
+        sariKova=bosKova;
+        System.out.println("Kırmızı Kovanın yeni degeri: "+kirmiziKova
+        +"\nSari kovanın yeni degeri : "+ sariKova);
+
     }
 }
