e5626e5b34d7dbae2cb919cba7476058322a6f4a
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-24 16:00:37 +0200

    java

--- src/day04/C07_Odev.java
+++ src/day04/C07_Odev.java
@@ -1,5 +1,7 @@
 package day04;
 
+import java.util.Scanner;
+
 public class C07_Odev {
     public static void main(String[] args) {
 
@@ -9,6 +11,27 @@ public class C07_Odev {
         olabilirsin” veya “Emekli olmak icin .. Yil daha calisman gerekir” yazdirin.
          */
 
-        System.out.println("Orhanhan github ogreniyor");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Cinsiyetinizi ve yasinizi girin");
+        String cinsiyet=scan.next();
+        int yas = scan.nextInt();
+
+
+        if (cinsiyet.contains("erkek")){
+            if (yas>=65){
+                System.out.println("Emekli olabilirsiniz");
+            }else {
+                System.out.println("Emekli olmaniza daha "+ (65-yas) + " yil var");
+            }
+        }
+        else {
+            if (yas>=60){
+                System.out.println("Emekli olabilirsiniz");
+            }
+        else {
+                System.out.println("Emekli olmaniza daha "+ (60-yas) + " yil var");
+            }
+        }
+
     }
 }
---
+++ src/day05/C01_IfSoru.java
@@ -0,0 +1,8 @@
+package day05;
+
+public class C01_IfSoru {
+    public static void main(String[] args) {
+        // Kullanicidan bir ucgenin 3 kenar uzunlugunu alin,
+        //          ucgen eskenar ise “Eskenar ucgen” yazdirin ve ucgenin alanini yazdirin
+    }
+}
