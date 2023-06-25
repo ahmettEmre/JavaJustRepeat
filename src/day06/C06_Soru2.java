0e7f1f8fd89fb31a795601f7112c82103bb3ce1d
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-25 17:47:53 +0200

    java

--- src/day06/C06_Soru2.java
+++ src/day06/C06_Soru2.java
@@ -1,9 +1,18 @@
 package day06;
 
+import java.util.Scanner;
+
 public class C06_Soru2 {
     public static void main(String[] args) {
         // Kullanicidan notunu alin 50 veya daha buyukse ”Sinifi Gectin”,
         // 50’den kucukse “Maalesef kaldin” yazdirin.
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lütfen notunuzu giriniz");
+
+        int not = scan.nextInt();
+
+        System.out.println( not>=50 ? "Sinifi Gectin" : " Maalesef kaldin " );
     }
 }
---
+++ src/day06/C07_Odev.java
@@ -0,0 +1,9 @@
+package day06;
+
+public class C07_Odev {
+
+    //Kullanicidan bir sayti alin
+    //sayi 7 ye tam bolunuyorsa "Girmis oldugunuz sayi 7 ye bolunuyor"
+    // bolunmuyor ise "Girmis oldugunuz sayi 7 ye bolunmuyor" yazdirin
+    //Hem ternary ile hem if-else ile cozun
+}
