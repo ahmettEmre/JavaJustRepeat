7a9f3648c8f7a75168b0003bff7cdf760b7a3c2f
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-25 17:10:16 +0200

    java

--- src/day06/C02_IfelseSoru.java
+++ src/day06/C02_IfelseSoru.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Scanner;
+
 public class C02_IfelseSoru {
     public static void main(String[] args) {
         /* kullanicidan bir tamsayi alin
@@ -9,5 +11,15 @@ public class C02_IfelseSoru {
            sayi 2 basamaktan buyukse "buyuk sayi" yazdirin
 
          */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir tam sayi giriniz");
+        int girilensayi = scan.nextInt();
+
+        if (girilensayi<0) System.out.println("negatif sayi");
+        else if (girilensayi >= 0 && girilensayi >= 9) System.out.println("girilen sayi rakam");
+        else if (girilensayi>=10 && girilensayi>=99) System.out.println("girilen sayi 2 basamakli");
+        else if (girilensayi>=100) System.out.println("buyuk sayi");{
+        }
     }
 }
