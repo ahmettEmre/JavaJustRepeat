f987e689560211bca44eabd8370c2584197c3791
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-06-25 17:36:58 +0200

    java

--- src/day06/C05_Sorular.java
+++ src/day06/C05_Sorular.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Scanner;
+
 public class C05_Sorular {
     public static void main(String[] args) {
 
@@ -8,5 +10,11 @@ public class C05_Sorular {
         sayi 10 dan küçük ise "kucuk sayi"
         sayi 10 yada 10 dan buyuk ise "buyuk sayi" yazirin
          */
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Bir sayi giriniz");
+        int sayi= scan.nextInt();
+
+        System.out.println(sayi<10 ? "kucuk sayi" :  "buyuk sayi");
     }
 }
---
+++ src/day06/C06_Soru2.java
@@ -0,0 +1,9 @@
+package day06;
+
+public class C06_Soru2 {
+    public static void main(String[] args) {
+        // Kullanicidan notunu alin 50 veya daha buyukse ”Sinifi Gectin”,
+        // 50’den kucukse “Maalesef kaldin” yazdirin.
+
+    }
+}
