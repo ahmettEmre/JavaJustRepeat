04a56a9a44a85f9f6de7a47e9e3189002ebcfc3d
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-07-02 19:37:12 +0200

    java

---
+++ src/day09/C07_odev.java
@@ -0,0 +1,33 @@
+package day09;
+
+public class C07_odev {
+    public static void main(String[] args) {
+
+        /*
+        Gittigimiz web sayfasindan aldigimiz
+        String arama sonuclarinda
+        arama sayisinin 50'den az oldugunu test edin
+
+
+        String input = "1-48 arasindaki 46 sonuc icin \"nutella\"";
+
+        -----------------------------------------------------
+
+        // 10 kere "OrhanHan" yazdiralim
+        // ama her yazisinda 1'den 10'a kadar(10 dahil) sira no ile yazdiralim
+
+        -------------------------------------------------------
+
+        // Kullanicinin verdigi sayiyi, bire kadar tum sayilari yazdirin
+        (NOT: Eger girilen sayi negatif ise "Gecersiz sayi diyin")
+
+        ---------------------------------------------------------
+
+         Kullanicidan pozitif bir sayi alin,
+        //       1’den baslayarak sayiya kadar tum tamsayilari yazdirin(sayi dahil), sira
+        //	- 3 ile bolunebilen bir sayiya gelirse, sayi yerine fizz
+        //	- 5 ile bolunebilen bir sayiya gelirse sayi yerine buzz
+        //	- hem 3 hem 5 ile bolunebilen bir sayiya gelirse sayi yerine fizzBuzz yazdirin
+         */
+    }
+}
